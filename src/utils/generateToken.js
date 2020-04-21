b2d2b50b9c7db8824815c4ef0948273292b319a4
francis-mwas <mwangif184@gmail.com>
2020-04-22 00:05:00 +0300

    Added secret key to .env file

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 const generateToken = (userId) => {
-  const token = jwt.sign({ id: userId }, "mysecret", {
+  const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
     expiresIn: "7 days",
   });
   return token;
